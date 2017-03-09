89d191b1ce6819349b6fdc1dea4e5f1698922b5d
Mike Wendt <mike.wendt@me.com>
2017-03-09 00:08:34 -0500

    added close, list, and listfrom commands

--- redminebot.py
+++ redminebot.py
@@ -33,6 +33,15 @@ def handle_command(command, channel, username):
             response = create_issue(newmsg, username, assigneduser)
         elif operator == "issue" and len(commands) > 1:
             response = create_issue(msg, username, username)
+        elif operator == "close" and len(commands) > 2:
+            issue = commands[1]
+            newmsg = s.join(commands[2:])
+            response = close_issue(newmsg, issue, username)
+        elif operator == "list":
+            response = list_issues(username)
+        elif operator == "listfrom" and len(commands) > 1:
+            listuser = commands[1]
+            response = list_issues(listuser) 
         elif operator == "help":
             response = show_commands()
     message = "@" + username + " " + response
@@ -62,26 +71,58 @@ def show_commands():
         Return ist of commands that bot can handle
     """
     return ":wrench: List of supported commands:\n" \
-           "`issue <subject>` - creates new issue and assigns it to yourself\n" \
-	   "`issueto <name> <subject>` - creates new issue and assigns it to `<user>`"
+           "`issue <subject>` - creates new issue and assigns it to you\n" \
+	   "`issueto <name> <subject>` - creates new issue and assigns it to `<name>`\n" \
+           "`close <issue #> <comment>` - closes an issue with the following comment\n" \
+           "`list` - list all open issues assigned to you\n" \
+           "`listfrom <name>` - list all open issues assigned to `<name>`"
 
 """
     Redmine commands
 """
 def create_issue(text, username, assigneduser):
-        user = rc.user.filter(name=username)[0]
+    user = rc.user.filter(name=username)[0]
     assigned = rc.user.filter(name=assigneduser)[0]
     if user.id == None or assigned.id == None:
-            return ":x: Failed to find your user name in Redmine"
+        return ":x: Failed to find your user name in Redmine"
     # impersonate user so it looks like the update is from them
     rcn = Redmine(os.environ.get('REDMINE_HOST'), version=os.environ.get('REDMINE_VERSION'), \
      key=os.environ.get('REDMINE_TOKEN'), impersonate=user.login)
     issue = rcn.issue.create(project_id='general', subject=text, tracker_id=2, assigned_to_id=assigned.id)
-        if issue.id:
-            return ":white_check_mark: Created Issue <"+EXT_HOST+"/issues/"+str(issue.id)+ \
+    if issue.id:
+        return ":white_check_mark: Created Issue <"+EXT_HOST+"/issues/"+str(issue.id)+ \
         "|#"+str(issue.id)+" "+issue.subject+"> assigned to "+assigned.firstname+" "+assigned.lastname
-        else:
-            return ":x: Issue creation failed"
+    else:
+        return ":x: Issue creation failed"
+    
+def list_issues(username):
+    user = rc.user.filter(name=username)[0]
+    if user.id == None:
+        return ":x: Failed to find your user name in Redmine"
+    result = rc.issue.filter(sort='project', assigned_to_id=user.id, status_id='open')
+    response = ""
+    if len(result) > 0:
+        response = ":book: Open issues assigned to "+user.firstname+" "+user.lastname+":\n"
+        for issue in result:
+            response += ""+issue.project.name+" <"+EXT_HOST+"/issues/"+str(issue.id)+"|#"+str(issue.id)+ \
+                        " "+issue.subject+">\n"
+    else:
+        response = ":thumbsup_all: No open issues assigned to "+user.firstname+" "+user.lastname
+    return response
+
+def close_issue(text, issue, username):
+    user = rc.user.filter(name=username)[0]
+    if user.id == None:
+        return ":x: Failed to find your user name in Redmine"
+    # impersonate user so it looks like the update is from them
+    rcn = Redmine(os.environ.get('REDMINE_HOST'), version=os.environ.get('REDMINE_VERSION'), \
+     key=os.environ.get('REDMINE_TOKEN'), impersonate=user.login)
+    result = rcn.issue.update(issue, status_id=5, notes=text, done_ratio=100)
+    if result:
+        return ":white_check_mark: Closed Issue <"+EXT_HOST+"/issues/"+str(issue)+ \
+        "|#"+str(issue)+">"
+    else:
+        return ":x: Issue closing failed"
 
 if __name__ == "__main__":
     READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
