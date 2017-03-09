a913a73ff33045806df6848b838ef2ad14911312
Mike Wendt <mike.wendt@me.com>
2017-03-08 22:38:52 -0500

    current working version

--- README.md
+++ README.md
@@ -1,2 +1,56 @@
 # redmine-slackbot
 Slackbot for Redmine to add issues and assign them to yourself or others
+
+## Install
+
+```
+pip install virtualenv
+virtualenv env
+source env/bin/activate
+pip install slackclient
+pip install python-redmine
+```
+
+## Configure
+
+### Slack
+
+1. Get token from Slack API for bot and save 
+ 
+ `export BOT_TOKEN="<slack api token>"`
+ 
+2. Run the following Python scrip to get the bot's user ID for `BOT_ID`
+ 
+ `python print_bot_id.py`
+
+### Redmine
+
+1. Get Redmine version from http://redmine/admin/info
+2. Get API key from user profile (may have to enable rest API in Admin if not seen)
+
+### Saving config
+
+1. Copy `run.sh.example` to `run.sh` 
+
+ `cp run.sh.example run.sh`
+
+2. Edit `run.sh` and fill in the following variables for your environment
+```
+export REDMINE_HOST="http://localhost"
+export REDMINE_EXT_HOST="http://172.0.0.1"
+export REDMINE_VERSION="3.3.1"
+export REDMINE_TOKEN="<redmine api token>"
+export BOT_ID="<from print_bot_id.py>"
+export BOT_TOKEN="<slack api token>"
+```
+
+## Running
+
+```
+bash run.sh
+```
+Recommended to use `supervisor` if you want a long running service that is fault tolerant
+
+## Source
+
+Thanks to https://www.fullstackpython.com/blog/build-first-slack-bot-python.html for the great starting point/guide
---
+++ redminebot.py
@@ -0,0 +1,97 @@
+import os
+import time
+from slackclient import SlackClient
+from redmine import Redmine
+
+# redminebot's ID and external host from environment variables
+BOT_ID = os.environ.get('BOT_ID')
+EXT_HOST = os.environ.get('REDMINE_EXT_HOST')
+
+# constants
+AT_BOT = "<@" + BOT_ID + ">"
+
+# instantiate Slack & Redmine clients
+sc = SlackClient(os.environ.get('BOT_TOKEN'))
+rc = Redmine(os.environ.get('REDMINE_HOST'), version=os.environ.get('REDMINE_VERSION'), \
+     key=os.environ.get('REDMINE_TOKEN'))
+
+def handle_command(command, channel, username):
+    """
+        Receives commands directed at the bot and determines if they
+        are valid commands. If so, then acts on the commands. If not,
+        returns back what it needs for clarification.
+    """
+    response = ":question: Unknown/invalid command - Try `help` for a list of supported commands"
+    commands = command.split()
+    if commands:
+        operator = commands[0]
+        s = " "
+        msg = s.join(commands[1:])
+        if operator == "issueto" and len(commands) > 2:
+            assigneduser = commands[1]
+            newmsg = s.join(commands[2:])
+            response = create_issue(newmsg, assigneduser)
+        elif operator == "issue" and len(commands) > 1:
+            response = create_issue(msg, username)
+        elif operator == "help":
+            response = show_commands()
+    message = "@" + username + " " + response
+    sc.api_call("chat.postMessage", channel=channel, \
+                          text=message, as_user=True)
+
+
+def parse_slack_output(slack_rtm_output):
+    """
+        The Slack Real Time Messaging API is an events firehose.
+        this parsing function returns None unless a message is
+        directed at the Bot, based on its ID.
+    """
+    output_list = slack_rtm_output
+    if output_list and len(output_list) > 0:
+        for output in output_list:
+            if output and 'text' in output and AT_BOT in output['text']:
+                profile = sc.api_call("users.info", user=output['user'])
+                username = profile['user']['name']
+                # return text after the @ mention, whitespace removed
+                return output['text'].split(AT_BOT)[1].strip().lower(), \
+                       output['channel'], username
+    return None, None, None
+
+def show_commands():
+    """
+        Return ist of commands that bot can handle
+    """
+    return ":wrench: List of supported commands:\n" \
+           "`issue <subject>` - creates new issue and assigns it to yourself\n" \
+	   "`issueto <name> <subject>` - creates new issue and assigns it to `<user>`"
+
+"""
+    Redmine commands
+"""
+def create_issue(text, username):
+    if text == None or text == "":
+        return ":exclamation: Missing subject to create issue; try again"
+    else:
+        user = rc.user.filter(name=username)[0]
+        if user.id == None:
+            return ":x: Failed to find your user name in Redmine"
+        issue = rc.issue.create(project_id='general', subject=text, tracker_id=2, assigned_to_id=user.id)
+        if issue.id:
+            return ":white_check_mark: Created Issue <"+EXT_HOST+"/issues/"+str(issue.id)+ \
+	    "|#"+str(issue.id)+" "+issue.subject+"> assigned to "+user.firstname+" "+user.lastname
+        else:
+            return ":x: Issue creation failed"
+
+if __name__ == "__main__":
+    READ_WEBSOCKET_DELAY = 1 # 1 second delay between reading from firehose
+    if sc.rtm_connect():
+        print("RedmineBot connected and running!")
+        while True:
+            command, channel, username = parse_slack_output(sc.rtm_read())
+            if command and channel and username:
+                handle_command(command, channel, username)
+            elif channel and username:
+                handle_command("", channel, username)
+            time.sleep(READ_WEBSOCKET_DELAY)
+    else:
+        print("Connection failed. Invalid Slack token or bot ID?")
