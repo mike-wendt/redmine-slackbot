8a6c2a40f5e42995959c2830dac32e4f147b4f5b
Mike Wendt <mike.wendt@me.com>
2018-04-27 18:40:31 -0400

    ENH List watchers after modifcations

--- redminebot.py
+++ redminebot.py
@@ -773,7 +773,10 @@ def cmd_watcher_add(text, issueid, username, watchername):
         else:
             text = log
         rm_update_issue(issue=issue.id, notes=text, rcn=rcn, estimate=estimate, record=record, percent=percent)
-        return ":eyes: Added _"+watcher.firstname+" "+watcher.lastname+"_ as watcher to "+issue_subject_url(issue.id,issue.subject)+comment
+        watchers = issue_watchers(issue)
+        if watchers != "":
+            watchers = "\n*Watchers:*\n> "+watchers+"\n"
+        return ":eyes: Added _"+watcher.firstname+" "+watcher.lastname+"_ as watcher to "+issue_subject_url(issue.id,issue.subject)+comment+watchers
     except:
         traceback.print_exc(file=sys.stderr)
         raise RuntimeError(":x: Watcher addition failed")
@@ -798,7 +801,10 @@ def cmd_watcher_delete(text, issueid, username, watchername):
         else:
             text = log
         rm_update_issue(issue=issue.id, notes=text, rcn=rcn, estimate=estimate, record=record, percent=percent)
-        return ":eyes: Removed _"+watcher.firstname+" "+watcher.lastname+"_ as watcher from "+issue_subject_url(issue.id,issue.subject)+comment
+        watchers = issue_watchers(issue)
+        if watchers != "":
+            watchers = "\n*Watchers:*\n> "+watchers+"\n"
+        return ":eyes: Removed _"+watcher.firstname+" "+watcher.lastname+"_ as watcher from "+issue_subject_url(issue.id,issue.subject)+comment+watchers
     except:
         traceback.print_exc(file=sys.stderr)
         raise RuntimeError(":x: Watcher deletion failed")
